986651809aeb1ccf302a7cb6d3a1930d5ddb83f1
AzeDonLees <ulvih99@gmail.com>
2021-12-08 19:30:05 +0400

    Test

---
+++ HW1/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HW_1"
+include(":androidApp")
+include(":shared")
---
+++ HW1/shared/src/androidTest/kotlin/com/example/hw_1/androidTest.kt
@@ -0,0 +1,12 @@
+package com.example.hw_1
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AndroidGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greeting().contains("Android"))
+    }
+}
